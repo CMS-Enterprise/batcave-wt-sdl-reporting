62cee61c8902b6019e58c444d8fa99c333f3536f
rileydakota <dakotariley2@gmail.com>
2024-01-17 11:48:16 -0500

    update diagram formatting

--- diagram.py
+++ diagram.py
@@ -5,10 +5,7 @@ from diagrams.aws.management import CloudwatchEventTimeBased
 from diagrams.saas.analytics import Snowflake
 from diagrams.saas.chat import Slack
 
-graph_attr = {
-    "fontsize": "25",
-    "center": "true"
-}
+graph_attr = {"fontsize": "25", "center": "true"}
 
 with Diagram("BatCAVE SDL Reporting Automation", direction="TB", graph_attr=graph_attr):
     with Cluster("AWS"):
@@ -19,22 +16,17 @@ with Diagram("BatCAVE SDL Reporting Automation", direction="TB", graph_attr=grap
             snowflake_secret = SecretsManager("Snowflake Creds")
 
         cron >> lambda_func
-        lambda_func \
-        >> Edge(label='GetSecretValue', color='red', style='bold') \
-        >> slack_secret
-        
-        lambda_func \
-        >> Edge(label='GetSecretValue', color='red') \
-        >> snowflake_secret
+        (
+            lambda_func
+            >> Edge(label="GetSecretValue", color="red", style="bold")
+            >> slack_secret
+        )
 
+        lambda_func >> Edge(label="GetSecretValue", color="red") >> snowflake_secret
 
-    snowflake = Snowflake('Security Data Lake')
-    slack = Slack('Slack')
+    snowflake = Snowflake("Security Data Lake")
+    slack = Slack("Slack")
 
-    lambda_func \
-        >> Edge(label="Execute Queries", color="green") \
-        >> snowflake
-    
-    lambda_func \
-        >> Edge(label="POST report to webhook", color="green") \
-        >> slack
+    lambda_func >> Edge(label="Execute Queries", color="green") >> snowflake
+
+    lambda_func >> Edge(label="POST report to webhook", color="green") >> slack
